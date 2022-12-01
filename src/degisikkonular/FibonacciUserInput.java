d16464b8c0ebfb75d080c4c42a600fd0425ad339
sagun33 <salihguenay33@gmail.com>
2022-12-01 23:54:12 +0100

    k

--- src/degisikkonular/FibonacciUserInput.java
+++ src/degisikkonular/FibonacciUserInput.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 
 public class FibonacciUserInput {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         int n1=0;
         int n2=1;
         int n3;
         int i;
         int n;
         System.out.print("n Sayısını Girin:");
-        n = reader.nextInt();
+
+        n = input.nextInt();
         System.out.print(n1+" "+n2);//0 ve 1 yazdır
 
         for(i=2;i<n;++i)//Döngü 2 den başlıyor çünkü ilk 2 terim 0 ve 1 her zaman yazılacak
